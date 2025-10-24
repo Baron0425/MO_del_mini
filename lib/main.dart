d3fd8b407bdb2685fc5a5603ee73d707ace491f2
premserround <well4581@gmail.com>
2025-10-24 14:15:15 +0700

    yoyo

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Deliveries',
       // theme: AppTheme.light,
       // darkTheme: AppTheme.dark,
       home: const LoginPage(),
